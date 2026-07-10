a54fbd00ff9c704591de746c2f951534d8d8f5fd
coolmanis1or2 <raymondzhou26@gmail.com>
2026-07-10 10:17:49 -0400

    hi

--- homepage.php
+++ homepage.php
@@ -8,6 +8,7 @@
         <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
             <div class="container">
                 <a class="navbar-brand" href="blackjack.html">My Site</a>
+                <a class="navbar-brand" href="blackrocrap.html">Secret</a>
 
 
             <div> 
